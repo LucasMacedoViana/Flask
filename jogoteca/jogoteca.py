5705fc3f891bb141ea16dfeff234050d9c6d9446
Lucas <lucasmacedo9@hotmail.com>
2022-07-11 21:47:27 -0300

    iniciando estudos sobre o flask

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, flash
 
 
 class Jogo:
@@ -13,28 +13,44 @@ jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 lista = [jogo1,jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 #para colocar uma aplicação precisamos criar uma rota e sempre é necessario colar uma função
-@app.route('/inicio')
-def ola():
+@app.route('/')
+def index():
     #para conectar ao arquivo HTML
     #O titulo é a variavel criada no html para que possamos mudar o html no python
     return render_template('lista.html', titulo = 'Jogos', jogos = lista )
 
-@app.route('/novo')
+@app.route('/novo' )
 def novo():
     return render_template('novo.html', titulo = 'Novo Jogo')
 
-@app.route('/criar')
+@app.route('/criar', methods = ['POST', ])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo = 'jogos', jogos = lista)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods = ['POST', ])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + 'Usuario logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuario não logado')
+        return redirect('/login')
 
 
 
-#isso faz rodar a apricação
-app.run()
+
+
+app.run(debug=True)
